69011977249cd45c1a5591750176088f04ee4214
Sophia K <e.s.klaussner@students.uu.nl>
2021-01-15 16:05:58 +0100

    Functioning total for all months!!! YEY!!

--- precipitationSeattle.py
+++ precipitationSeattle.py
@@ -14,11 +14,12 @@ with open ('precipitation.json', encoding='utf-8') as precipitation:
     rain=(json.load(precipitation))
 
 #Filter for station and its measurement:
-rain_seattle_month={}
+rain_seattle_month=[0,0,0,0,0,0,0,0,0,0,0,0] #So there is a position that we can later assign it to these positions.
 for measurement in rain:
     if 'GHCND:US1WAKG0038' in measurement['station']:
         month= int(measurement['date'].split('-')[1])
-        value= int(measurement['value'])
+        value=measurement['value']
+        rain_seattle_month[month-1]+=value #since the value for months starts at 1, but we want to start at 0.
+print(rain_seattle_month)
 
-#Try to filter for 
-# {1: value, 2: value, ...}
+
